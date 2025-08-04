632fa62f2082676b6015879c271d5682d737395f
matitooo <dagostinimattia0@gmail.com>
2025-08-04 17:19:31 +0200

    Added preprocessing and graph creation

---
+++ download_preprocess.py
@@ -0,0 +1,76 @@
+import os
+import shutil
+from nilearn import datasets
+from ABIDE_utils import ABIDEParser as Reader
+import yaml
+
+
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+
+# Configuration
+pipeline = config["pipeline"]
+n_subjects = config["n_subjects"]
+atlas_name = config["atlas_name"]
+kind = config["kind"]
+
+data_folder = os.path.join(root_folder, f"ABIDE_pcp/{pipeline}/filt_noglobal")
+
+filemapping = {"rois_cc200": "rois_cc200.1D"}
+
+# 1. Create data directory
+os.makedirs(data_folder, exist_ok=True)
+shutil.copyfile(
+    "ABIDE_utils/subject_IDs.txt", os.path.join(data_folder, "subject_IDs.txt")
+)
+
+
+# 2. Data download
+abide = datasets.fetch_abide_pcp(
+    data_dir=root_folder,
+    n_subjects=n_subjects,
+    pipeline=pipeline,
+    band_pass_filtering=True,
+    global_signal_regression=False,
+    derivatives=["rois_cc200"],
+)
+
+# Organizing data per subject
+subject_IDs = Reader.get_ids(n_subjects)
+subject_IDs = subject_IDs.tolist()
+
+for s, fname in zip(
+    subject_IDs, Reader.fetch_filenames(subject_IDs, f"rois_{atlas_name}")
+):
+    subject_folder = os.path.join(data_folder, s)
+    os.makedirs(subject_folder, exist_ok=True)
+
+    base = fname.split(f"rois_{atlas_name}")[0]
+    src = base + filemapping[f"rois_{atlas_name}"]
+    dst = os.path.join(subject_folder, base + filemapping[f"rois_{atlas_name}"])
+    if not os.path.exists(dst):
+        shutil.move(src, dst)
+
+# Time series extraction
+time_series = Reader.get_timeseries(subject_IDs, atlas_name)
+
+
+# Connectivity matrix creation
+for i, ts in enumerate(time_series):
+    subject_id = subject_IDs[i]
+    conn = Reader.subject_connectivity(ts, subject_id, atlas_name, kind, save=False)
+
+    save_path = os.path.join(
+        data_folder, subject_id, f"{subject_id}_{atlas_name}_{kind}.mat"
+    )
+    Reader.sio.savemat(save_path, {"connectivity": conn})
+
+folders = [name for name in os.listdir(data_folder)]
+folders.sort()
+
+with open("subject_IDs.txt", "w") as f:
+    for folder in folders:
+        f.write(f"{folder}\n")
+
+print(f"{len(folders)} found!")
+print("Preprocessing completed")
---
+++ graph_creation_utils.py
@@ -0,0 +1,140 @@
+import numpy as np
+import scipy.io as sio
+import os
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def matrix_preprocessing(data_folder,phenotype_file,subject_ids_file,atlas_name,kind):
+
+    subject_ids = np.genfromtxt(subject_ids_file, dtype=str)
+    
+    # Load connectivity matrixes
+
+    features = []
+    eps = 1e-5
+    for sid in subject_ids:
+        try:
+            mat_file = os.path.join(data_folder, str(sid), f"{sid}_{atlas_name}_{kind}.mat")
+            mat = sio.loadmat(mat_file)["connectivity"]
+            mat = np.clip(mat, -1 + eps, 1 - eps)
+            mat = np.arctanh(mat)  # Fisher z-transform
+            upper_tri = mat[np.triu_indices_from(mat, k=1)]
+            features.append(upper_tri)
+        except Exception as e:
+            print(f"Error on subject {sid}: {e}")
+    
+    pheno_df = pd.read_csv(phenotype_file)
+    pheno_df = pheno_df[pheno_df["SUB_ID"].isin(subject_ids.astype(int))]
+
+    # Label encoding
+    label_encoder = LabelEncoder()
+    labels = []
+
+    for sid in subject_ids:
+        diagnosis = pheno_df.loc[pheno_df["SUB_ID"] == sid, "DX_GROUP"].values[0]
+        labels.append(diagnosis)
+
+    y = label_encoder.fit_transform(labels)  # 1 = ASD, 0 = Control
+    return features,y
+
+def graph_creation(data_folder,phenotype_file,subject_ids_file,atlas_name,kind):
+    """
+    Create adjacency matrix given a subject ID list from the ABIDE dataset. 
+    Current phenotipes used for correlation are:
+        -Age
+        -Sex
+    """
+
+    # List of subjects loading
+    subject_ids = np.genfromtxt(subject_ids_file, dtype=str).astype(int)
+
+    # Phenotipes loading
+
+    pheno_df = pd.read_csv(phenotype_file)
+    pheno_df = pheno_df[pheno_df["SUB_ID"].isin(subject_ids.astype(int))]
+
+    # Label encoding
+    label_encoder = LabelEncoder()
+    labels = []
+
+    for sid in subject_ids:
+        diagnosis = pheno_df.loc[pheno_df["SUB_ID"] == sid, "DX_GROUP"].values[0]
+        labels.append(diagnosis)
+
+    y = label_encoder.fit_transform(labels)  # 1 = ASD, 0 = Control
+    print(f"Labels shape: {y.shape}")
+
+    """
+    Phenotipic features extraction and graph creation
+
+    Features used
+    - Age
+    - Sex
+
+    Features are normalized and cosine similarity is computed
+    """
+
+    #Features array creation
+    ages = []
+    sexes = []
+
+    for sid in subject_ids:
+        row = pheno_df[pheno_df["SUB_ID"] == sid].iloc[0]
+        ages.append(row["AGE_AT_SCAN"])
+        sexes.append(row["SEX"])  # 1 = Male, 2 = Female
+
+    ages = np.array(ages).reshape(-1, 1)
+    sexes = np.array(sexes).reshape(-1, 1)
+
+    # Age normalization
+    scaler = StandardScaler()
+    ages_norm = scaler.fit_transform(ages)
+
+    # Converting sex into binary values
+    sex_bin = (sexes == 1).astype(int)
+
+    # Combined vector creation
+    phenotype_features = np.hstack([ages_norm, sex_bin])
+
+    # Similarity matrix computation
+    sim_matrix = cosine_similarity(phenotype_features)
+
+    # Using treshold and computing adjacency
+    threshold = 0.8
+    adjacency = (sim_matrix >= threshold).astype(int)
+
+    # Excluding self-loop
+    np.fill_diagonal(adjacency, 0)
+
+    print(f"Population graph adjacency matrix shape: {adjacency.shape}")
+    print(f"Number of edges: {adjacency.sum() // 2}")
+
+    """
+    GRAPH VISUALIZATION (OPTIONAL)
+    """
+
+    # G = nx.from_numpy_array(adjacency)
+
+    # plt.figure(figsize=(10, 8))
+    # pos = nx.spring_layout(G, seed=42)
+
+    # """
+    # Graph Legend
+    # - Red = ASD
+    # - Blue = Control
+    # """
+    # nx.draw_networkx_nodes(
+    #     G, pos, node_size=50, node_color=y, cmap=plt.cm.Set1, label="ASD=Red, Control=Blue"
+    # )
+
+    # nx.draw_networkx_edges(G, pos, alpha=0.3)
+
+    # plt.title("Population Graph based on phenotipes: age and sex")
+    # plt.axis("off")
+    # plt.show()
+    
+    return adjacency
